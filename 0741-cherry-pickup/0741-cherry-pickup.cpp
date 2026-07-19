2cbf649e78b11350fb922df37f889f0ce3ccbde4
Khushboo Gond <kgond497@gmail.com>
2026-07-19 14:55:20 +0530

    Time: 37 ms (73.29%), Space: 27.6 MB (62.67%) - LeetHub

---
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -0,0 +1,50 @@
+class Solution {
+private:
+    vector<vector<int>> grid;
+    int n;
+    vector<vector<vector<int>>> memo;
+
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        this->grid=grid;
+        n=grid.size();
+
+        memo.assign(n,vector<vector<int>>(n,vector<int>(n,-1)));
+
+        return max(0,dfs(0,0,0));
+    }
+
+private:
+    int dfs(int r1,int c1,int r2){
+        int c2=r1+c1-r2;
+
+        if(r1>=n||c1>=n||r2>=n||c2>=n){
+            return -1e9;
+        }
+
+        if(grid[r1][c1]==-1||grid[r2][c2]==-1){
+            return -1e9;
+        }
+
+        if(r1==n-1&&c1==n-1){
+            return grid[r1][c1];
+        }
+
+        if(memo[r1][c1][r2]!=-1){
+            return memo[r1][c1][r2];
+        }
+
+        int cherries=grid[r1][c1];
+
+        if(r1!=r2 || c1!=c2){
+            cherries+=grid[r2][c2];
+        }
+
+        int best=max(max(dfs(r1+1,c1,r2+1),dfs(r1+1,c1,r2)),
+                    max(dfs(r1,c1+1,r2+1),dfs(r1,c1+1,r2)));
+
+        cherries+=best;
+
+        return memo[r1][c1][r2]=cherries;
+    }
+};
