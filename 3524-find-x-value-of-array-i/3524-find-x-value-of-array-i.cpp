423f28cb9e800da17b69c25f94729a0a0c8307ee
Khushboo Gond <kgond497@gmail.com>
2026-09-21 19:37:41 +0530

    Time: 167 ms (41.07%), Space: 151.1 MB (70.54%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        vector<long long> ans(k,0);
+        vector<long long> dp(k,0);
+
+        for(int x:nums){
+            vector<long long> ndp(k,0);
+
+            int val=x%k;
+            ndp[val]++;
+
+            for(int r=0;r<k;r++){
+                int newR=(r*val)%k;
+                ndp[newR]+=dp[r];
+            }
+
+            for(int r=0;r<k;r++){
+                ans[r]+=ndp[r];
+            }
+            dp=ndp;
+        }
+        return ans;
+    }
+};
